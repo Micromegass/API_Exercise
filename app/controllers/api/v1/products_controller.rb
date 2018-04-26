40fb1aa0e50b58796dfefe04f1f20a349d004901
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-04-26 10:42:17 -0500

    added functionality to add products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::ProductsController < ApplicationController
     @products = Product.all
     render json: @products
   end
+
+
+def create
+  @product = Product.new
+  if @product.save
+  render json: @product
+  else
+  render json: {errors: product.errors}, status: 422
+  end
+end
+
+
 end
